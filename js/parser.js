2ddf1d67a0ffff7fc5704af28994b903d686e792
Graham Still <gstill@uw.edu>
2019-07-29 16:54:16 -0500

    Add nested loops

---
+++ js/parser.js
@@ -0,0 +1,121 @@
+const BEGIN_LOOP = '[';
+const END_LOOP = ']';
+const END_OF_LINE = ';';
+
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
+
+RegExp.escape = function (s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+class Parser {
+  constructor() {
+    this.PATTERNS = {
+      begin_loop: RegExp(`(\\d*)${RegExp.escape(BEGIN_LOOP)}`),
+      end_loop: RegExp(RegExp.escape(END_LOOP)),
+      end_of_line: RegExp(RegExp.escape(END_OF_LINE)),
+      pixel: /(\d*)([a-z]+)/
+    }
+  }
+
+  maxPattern() {
+    let patterns = [
+      this.PATTERNS['begin_loop'],
+      this.PATTERNS['end_loop'],
+      this.PATTERNS['end_of_line'],
+      this.PATTERNS['pixel'],
+      /S+/
+    ];
+
+    return new RegExp(patterns.map(p => p.source).join('|'), 'g');
+  }
+
+  parse(string) {
+    let tokens = string.matchAll(this.maxPattern());
+
+    let program = new Program();
+    let stack = [program];
+    let loop;
+
+    for (let [token] of tokens) {
+      if (this.PATTERNS['pixel'].test(token)) {
+        let [_, n, c] = token.match(this.PATTERNS['pixel']);
+        n = n || 1;
+        stack.last().add(new Pixel(parseInt(n), c));
+      } else if (this.PATTERNS['begin_loop'].test(token)) {
+        let [_, n] = token.match(this.PATTERNS['begin_loop']);
+        n = n || 1;
+        loop = new Loop(parseInt(n));
+        stack.last().add(loop);
+        stack.push(loop);
+      } else if (token === ']') {
+        if (!(stack.last() instanceof Loop)) {
+          throw new Error('Unexpected ]');
+        }
+        stack.pop();
+      } else if (token === ';') {
+        stack.last().add(new Newline());
+      } else {
+        throw new Error(`Unexpected token '${token}'`);
+      }
+    }
+
+    if (stack.length > 1) {
+      throw new Error('Unmatched [');
+    }
+
+    return program;
+  }
+}
+
+class Program {
+  constructor() {
+    this.commands = [];
+  }
+
+  add(command) {
+    this.commands.push(command);
+  }
+
+  toArray() {
+    return this.commands.reduce((arr, command) => arr.concat(command.toArray()), []);
+  }
+
+  length() {
+    return this.commands.length;
+  }
+}
+
+class Pixel {
+  constructor(number, clr) {
+    this.number = number;
+    this.clr = clr;
+  }
+
+  toArray() {
+    return Array(this.number).fill(getClr(this.clr));
+  }
+}
+
+class Loop {
+  constructor(number) {
+    this.number = number;
+    this.program = new Program();
+  }
+
+  add(command) {
+    this.program.add(command);
+  }
+
+  toArray() {
+    return Array(this.number).fill(this.program.toArray()).flat();
+  }
+}
+
+class Newline {
+  toArray() {
+    return [null];
+  }
+}
--- js/sketch.js
+++ js/sketch.js
@@ -10,6 +10,10 @@ let row = 0;
 // The current column to draw
 let col = 0;
 
+let programIndex = 0;
+
+let parser = new Parser();
+
 // How wide each pixel will be (calculated based on the GRID_SIZE and width)
 let pixelSize;
 
@@ -46,6 +50,7 @@ function prepareDrawing() {
   pixels = parse(getProgram());
   col = 0;
   row = 0;
+  programIndex = 0;
   clearScreen();
 }
 
@@ -64,56 +69,32 @@ function clearScreen() {
  * Draws the next available pixel
  */
 function drawNextPixel() {
-  // Don't draw anything if there's nothing left to draw
-  if (row >= pixels.length) {
+  while (programIndex < pixels.length && !pixels[programIndex]) {
+    row++;
+    col = 0;
+    programIndex++;
+  }
+
+  if (programIndex >= pixels.length) {
     return;
   }
 
-  // Draw the pixel
-  fill(pixels[row][col]);
+  fill(pixels[programIndex]);
   rect(col * pixelSize, row * pixelSize, pixelSize, pixelSize);
-
-  // Move the pointers
   col++;
-  if (col >= pixels[row].length) {
-    col = 0;
-    row++;
-  }
+  programIndex++;
 }
 
 /**
  * Parses a program into a nested array of pixels
  * 
- * @param  A program for drawing pixels
+ * @param string  A program for drawing pixels
  * @return  An array of arrays containing colours for each pixel
  */
-function parse(program) {
-  console.log(program);
-  return program.trim('\n')
-    .split('\n')
-    .map(line => line.split(/\s/)
-      .reduce((pixels, expr) => {
-        let [n, c] = parseExpr(expr);
-        return pixels.concat(Array(n).fill(c));
-      }, [])
-    );
-}
-
-/**
- * Parses a single expression from a program
- * 
- * Expressions take the form of [NUMBER][COLOUR CODE], where [NUMBER] is
- * optional. If [NUMBER] is omitted, it is assumed to be 1.
- * 
- * @param expr  The expression to parse
- * @return  An array, where the first element is the number of pixels,
- *          and the second is the colour of the pixels
- */
-function parseExpr(expr) {
-  let matchData = expr.match(/(\d*)(.+)/);
-  let n = parseInt(matchData[1] || 1);
-  let c = getClr(matchData[2]);
-  return [n, c];
+function parse(string) {
+  console.log(string);
+  let program = parser.parse(string.replace(/\n/g, ';'));
+  return program.toArray();
 }
 
 /**
