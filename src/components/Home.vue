2fee4a23ff105f9b7db64cfc4f860b79ca3dad3b
Graham Still <gstill@uw.edu>
2019-09-15 16:24:24 -0500

    Style tabs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    <div class="tabs">
+  <div class="home-component">
+    <div class="tabs is-boxed">
       <ul>
-        <li>
+        <li :class="{ 'is-active': isCodeRoute }">
           <router-link to="code">
             Code
           </router-link>
         </li>
-        <li>
+        <li :class="{ 'is-active': isAboutRoute }">
           <router-link to="about">
             About
           </router-link>
-        <li>
+        </li>
+        <li :class="{ 'is-active': isReferenceRoute }">
           <router-link to="ref">
             Reference
           </router-link>
@@ -19,9 +20,39 @@
       </ul>
     </div>
 
-    <router-view />
+    <div class="box">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  computed: {
+    path() {
+      return this.$route.path;
+    },
+
+    isCodeRoute() {
+      return this.path === '/' || this.path === '/code';
+    },
+
+    isAboutRoute() {
+      return this.path === '/about';
+    },
+
+    isReferenceRoute() {
+      return this.path === '/ref';
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+.home-component {
+  .tabs {
+    margin-bottom: 0 !important;
+    padding-left: 2em;
+  }
+}
+</style>
