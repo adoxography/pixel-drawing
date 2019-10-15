8ba7d6c6a38712e3edd6260f997ebc656f75e0ee
Graham Still <gstill@uw.edu>
2019-10-14 19:15:57 -0500

    Change the look of the toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="field">
-    <span class="control">
-      <a
-        v-for="(button, index) in buttons"
-        :key="index"
-        :title="button.title"
-        class="button icon"
-        @click="handleClick(button.action)"
+  <div class="field toolbar is-pulled-right">
+    <a
+      v-for="(button, index) in buttons"
+      :key="index"
+      :title="button.title"
+      class="button icon is-medium"
+      @click="handleClick(button.action)"
       >
-        <i
-          class="fas"
-          :class="'fa-'+button.icon"
-        ></i>
-      </a>
-    </span>
+      <i
+        class="fas"
+        :class="'fa-'+button.icon"
+      ></i>
+    </a>
   </div>
 </template>
 
@@ -33,3 +31,11 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.toolbar {
+  a:not(:first-child) {
+    margin-left: .2em;
+  }
+}
+</style>
