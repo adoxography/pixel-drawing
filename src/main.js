26f2e191069812c08f04eabdaa7261bf1a00ce7a
Graham Still <gstill@uw.edu>
2019-11-01 09:54:12 -0500

    Limit the number of icons that are included

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue';
 
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy';
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload';
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons/faFolderOpen';
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave';
+import { faUpload } from '@fortawesome/free-solid-svg-icons/faUpload';
 
 window.__VERSION__ = '[AIV]{version}[/AIV]';
 
@@ -15,7 +21,15 @@ import store from './store';
 require('@/sass/main.scss');
 
 // Set up fontawesome
-library.add(fas, fab);
+library.add(
+  faChevronRight,
+  faCopy,
+  faDownload,
+  faFolderOpen,
+  faPlus,
+  faSave,
+  faUpload
+);
 dom.watch();
 
 // Initialize Vue instance
