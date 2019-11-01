bcfd364aeb48adc0a225c4975edab6d4315ceb79
Graham Still <gstill@uw.edu>
2019-11-01 13:25:11 -0500

    Updates

---
+++ js/19.4a579b720e78469f2604.chunk.js
@@ -0,0 +1,2 @@
+// [AIV_SHORT]  Build version: 0.4.0 - Friday, November 1st, 2019, 9:56:39 AM  
+ (window.webpackJsonp=window.webpackJsonp||[]).push([[19],[],[["55Il",0,7,6,12]]]); 
---
+++ js/20.4a579b720e78469f2604.chunk.js
@@ -0,0 +1,2 @@
+// [AIV_SHORT]  Build version: 0.4.0 - Friday, November 1st, 2019, 9:56:39 AM  
+ (window.webpackJsonp=window.webpackJsonp||[]).push([[20],[],[["l4Qa",0,4,2,3,1]]]); 
